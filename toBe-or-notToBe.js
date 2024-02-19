f713b0989e9a16df5c3a4729c17adc63bc0287d0
cragtech <cragtechnology@gmail.com>
2024-02-19 11:32:39 -0700

    working function as decribed in prompt

--- toBe-or-notToBe.js
+++ toBe-or-notToBe.js
@@ -3,6 +3,21 @@
 // toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
 // notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal"
 
-const expect = () => {
-
+const expect = (val) => {
+  return {
+    toBe: (val2) => {
+      if (val === val2) {
+        return true;
+      } else {
+        throw new Error("Not Equal"); //throws error with message
+      }
+    },
+    notToBe: (val2) => {
+      if (val !== val2) {
+        return true;
+      } else {
+        throw new Error("Equal"); //throws error with message
+      }
+    }
+  };
 }
